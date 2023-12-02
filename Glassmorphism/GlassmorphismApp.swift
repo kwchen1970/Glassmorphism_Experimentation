eedab2d7f85af0fc0651f141bc4962df50903920
Kathy Chen <kc994@cornell.edu>
2023-12-02 18:52:13 -0500

    Initial Commit

---
+++ Glassmorphism/GlassmorphismApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassmorphismApp.swift
+//  Glassmorphism
+//
+//  Created by Kathleen Chen on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct GlassmorphismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
