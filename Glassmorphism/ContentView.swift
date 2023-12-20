fb6dd376a17237922a97f22c9263971bd7c61ce0
Kathy Chen <kc994@cornell.edu>
2023-12-19 23:54:31 -0500

    add content view back again

--- Glassmorphism/ContentView.swift
+++ Glassmorphism/ContentView.swift
@@ -8,96 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        NavigationView {
-            ScrollView{
-                Section {
-                    ScrollView(.horizontal) {
-                        ShonenView()
-                        
-                    }
-                } header: {
-                    Text("Shonen")
-                        .modifier(SectionHeaderStyling())
-                } .padding(.leading,10)
-                Section {
-                    ScrollView(.horizontal) {
-                        IsekaiView()
-                    }
-                } header: {
-                    Text("Isekai")
-                        .modifier(SectionHeaderStyling())
-                } .padding(.leading,10)
-                Section {
-                    ScrollView(.horizontal) {
-                        SliceofLifeView()
-                    }
-                } header: {
-                    Text("Slice of Life")
-                        .modifier(SectionHeaderStyling())
-                } .padding(.leading,10)
-                Section {
-                    ScrollView(.horizontal) {
-                        SportsView()
-                    }
-                } header: {
-                    Text("Sports")
-                        .modifier(SectionHeaderStyling())
-                } .padding(.leading,10)
-                Section {
-                    ScrollView(.horizontal) {
-                        RomanceView()
-                    }
-                } header: {
-                    Text("Romance")
-                        .modifier(SectionHeaderStyling())
-                }.padding(.leading,10)
-                Section {
-                    ScrollView(.horizontal) {
-                        PsychologicalView()
-                    }
-                } header: {
-                    Text("Psychological")
-                        .modifier(SectionHeaderStyling())
-                }.padding(.leading,10)
-                Section {
-                    ScrollView(.horizontal) {
-                        FantasyView()
-                    }
-                } header: {
-                    Text("Fantasy")
-                        .modifier(SectionHeaderStyling())
-                } .padding(.leading,10)
-                Section {
-                    ScrollView(.horizontal) {
-                        SciFiView()
-                    }
-                } header: {
-                    Text("Sci-Fi")
-                        .modifier(SectionHeaderStyling())
-                } .padding(.leading,10)
-                Section {
-                    ScrollView(.vertical) {
-                        AllView()
-                    }
-                    
-                .frame(height:700)
-                } header: {
-                    Text("All")
-                        .modifier(SectionHeaderStyling())
-                } .padding(.leading,10)
-            } 
-        }
+    @State var currentTab: Tab = .homes
+    
+    init() {
+        UITabBar.appearance().isHidden = true
     }
-}
-struct SectionHeaderStyling: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.headline)
-            .frame(maxWidth:.infinity,alignment:.leading)
-            .background{
-                Color.clear
+    var body: some View {
+        VStack(spacing: 0.0) {
+            TabView(selection: $currentTab) {
+                HomeTabView()
+                    .tag(Tab.homes)
+                BrowseTabView()
+                    .tag(Tab.browse)
+                SavedTabView()
+                    .tag(Tab.saved)
+                ProfileTabView()
+                    .tag(Tab.account)
+
             }
+            CustomTabBarView(currentTab:$currentTab)
+        }
+        
+        
     }
 }
 struct ContentView_Previews: PreviewProvider {
