34f72468d4389a4033938f45cd047cb5609a6304
Kathy Chen <kc994@cornell.edu>
2023-12-27 22:49:33 -0500

    fixed tabbar

--- Glassmorphism/Views/TabBarViews/CustomTabBarView.swift
+++ Glassmorphism/Views/TabBarViews/CustomTabBarView.swift
@@ -9,23 +9,34 @@ import SwiftUI
 
 struct CustomTabBarView: View {
     @Binding var currentTab: Tab
+    private var fillImage: String {
+        currentTab.systemImage + ".fill"
+    }
+    var backgroundColors = [Color(.systemPink),Color(.blue)]
     var body: some View {
-        HStack(spacing: 0.0) {
-            ForEach(Tab.allCases, id: \.rawValue) {tab in
-                Button {
-                    withAnimation(.easeInOut) {
-                        currentTab = tab
+        VStack {
+            HStack(spacing: 0.0) {
+                ForEach(Tab.allCases, id: \.rawValue) {tab in
+                    Button {
+                        withAnimation(.easeInOut(duration:0.2)) {
+                            currentTab = tab
+                        }
+                    } label: {
+                        Image(systemName: currentTab == tab ? fillImage: tab.systemImage)
+                     //   Image(systemName: tab.systemImage)
+                            .renderingMode(.template)
+                            .frame(maxWidth:.infinity)
+                            .foregroundColor(.white)
                     }
-                } label: {
-                    Image(systemName: tab.systemImage)
-                        .renderingMode(.template)
-                        .frame(maxWidth:.infinity)
-                        .foregroundColor(.white)
                 }
             }
+            .frame(maxWidth: .infinity)
         }
-        .frame(maxWidth: .infinity)
-        .background(.black)
+        .frame(height:20)
+        .padding(.top,20)
+        .background(.ultraThinMaterial)
+        .background(LinearGradient(colors: backgroundColors, startPoint:.leading, endPoint: .trailing))
+        
     }
 }
 
