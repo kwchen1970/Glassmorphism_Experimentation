ad575013210123776811007b11de47ac1aa9701a
Kathy <kwchen1970@gmail.com>
2024-05-25 00:48:56 -0400

    added to detail vc

--- Glassmorphism/Controllers/DetailVC.swift
+++ Glassmorphism/Controllers/DetailVC.swift
@@ -9,11 +9,23 @@ import SwiftUI
 
 struct DetailVC: View {
     var anime: Anime
+//    let width: CGFloat
+//    let height: CGFloat
     var body: some View {
         NavigationView{
-            VStack{
-                Text("Detail" + anime.name)
-            }
+            VStack(alignment: .leading, spacing:0){
+                Image(anime.image)
+                    .resizable()
+                    .frame(width: 400, height: 360)
+                    .aspectRatio(contentMode: .fill)
+                    .scaledToFill()
+                    .offset(y: -10)
+                    .edgesIgnoringSafeArea(.top)
+                Text(anime.name)
+                    .offset(y:-50)
+                
+            }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+            Spacer()
         }
     }
 }
